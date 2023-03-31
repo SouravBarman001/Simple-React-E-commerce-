1d437aca129c659d214574f1394f1da4f84240b0
souravbarman001 <sourav.iit.nstu@gmail.com>
2023-03-31 20:02:12 +0600

    fetch data

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,36 @@
+import React, { useState,useEffect } from 'react';
+import './Shop.css';
+import Product from '../Product/Product';
+
+const Shop = () => {
+   
+    const [products,setProducts] = useState([]);
+
+
+    useEffect(()=>{
+
+        fetch('products.json')
+        .then(res=> res.json())
+        .then(data => setProducts(data))
+
+    } ,[])
+
+    return (
+        <div className="shop-container">
+            <div className="products-container">
+                {
+                    products.map(product => <Product
+                    key={product.id}
+                    product={product}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+            <h2>products cart</h2>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
